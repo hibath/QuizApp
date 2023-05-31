f9e2be9af1ced9ef695e80546b719aaa72587ce3
hibaagha <Diadora123>
2023-05-31 11:16:00 +0200

    update constraints

--- QuizApp/Model/QuestionManager.swift
+++ QuizApp/Model/QuestionManager.swift
@@ -15,20 +15,20 @@ struct QuestionManager {
     
     let quiz = [
         Question(q: "Strawberries are among the fruits that contain the highest levels of pesticides.", a: "True"),
-        Question(q: "Too much fast food may not be associated with an increase in autoimmune diseases", a: "False"),
-        Question(q: "Vitamin D supports bone health and helps with calcium absorption", a: "True"),
+        Question(q: "Too much fast food may not be associated with an increase in autoimmune diseases.", a: "False"),
+        Question(q: "Vitamin D supports bone health and helps with calcium absorption.", a: "True"),
         Question(q: "Fiber-rich foods help maintain healthy cholesterol levels and promote heart health.", a: "True"),
-        Question(q: "Probiotics found in yogurt and fermented foods support your bones", a: "False"),
-        Question(q: "Zinc is not essential for immune function, wound healing, and DNA synthesis", a: "False"),
-        Question(q: "Canola oil is a highly healthy choice", a: "False"),
-        Question(q: "Antioxidants in berries protect cells from damage and promote overall wellness", a: "False"),
-        Question(q: "Olive oil is a very healthy choice", a: "True"),
+        Question(q: "Probiotics found in yogurt and fermented foods support your bones.", a: "False"),
+        Question(q: "Zinc is not essential for immune function, wound healing, and DNA synthesis.", a: "False"),
+        Question(q: "Canola oil is a highly healthy choice.", a: "False"),
+        Question(q: "Antioxidants in berries protect cells from damage and promote overall wellness.", a: "False"),
+        Question(q: "Olive oil is a very healthy choice.", a: "True"),
         Question(q: "Farmed salmon is healthier than wild salmon.", a: "False"),
-        Question(q: "Polyphenols in green tea have antioxidant and anti-inflammatory properties", a: "True"),
+        Question(q: "Polyphenols in green tea have antioxidant and anti-inflammatory properties.", a: "True"),
         Question(q: "Mixing turmeric with black pepper significantly increases its absorption rate.", a: "True"),
         Question(q: "Organic food has fewer pesticides", a: "True"),
         Question(q: "You don't have to wash and scrub organic fruits and vegetables well under running water.", a: "False"),
-        Question(q: "Iron-rich foods like spinach and lentils help prevent anemia and support energy levelss", a: "True")
+        Question(q: "Iron-rich foods like spinach and lentils help prevent anemia and support energy levelss.", a: "True")
         
     ]
     
